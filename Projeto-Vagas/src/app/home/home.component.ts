77cb1b442b8d6dac63b5c01ace7e32e36c205568
Pedro Porto <122691335+PedroPortoExo@users.noreply.github.com>
2025-02-08 17:26:39 -0300

    home , not-found pages e json com as vagas + app routes

---
+++ Projeto-Vagas/src/app/home/home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector:"home",
+    standalone: true,
+    imports: [],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
+})
+export class HomeComponent {}
